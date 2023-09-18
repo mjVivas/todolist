35c70083134f02fcd45e7520762f9c84a3c02320
mjVivas <132530990+mjVivas@users.noreply.github.com>
2023-09-18 15:41:42 -0400

    first commit

---
+++ src/TodoList.jsx
@@ -0,0 +1,19 @@
+import ToDoItem from "./ToDoItem";
+
+export default function TodoList({ todos, toggleTodo, deleteTodo }) {
+  return (
+    <ul className="list">
+      {todos.length === 0 && "No Todos"}
+      {todos.map((todo) => {
+        return (
+          <ToDoItem
+            {...todo}
+            key={todo.id}
+            toggleTodo={toggleTodo}
+            deleteTodo={deleteTodo}
+          />
+        );
+      })}
+    </ul>
+  );
+}
